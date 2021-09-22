a234de74dbd4ffd70617bba28ba99c1a894e9aac
Himanshu <Himanshu@robrosystems.com>
2021-09-22 23:43:51 +0530

    initial website

---
+++ assets/bootstrap/js/ros_bridge.js
@@ -0,0 +1,6 @@
+function Function(){
+	console.log("Ended");
+}
+document.getElementById("main_image").addEventListener("transitionend", Function);
+//document.getElementById("main_image").on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+
